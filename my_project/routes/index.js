7bdea35bc3abee6c538db70b04344bc04da61df4
ypy <1132250705@qq.com>
2018-02-26 20:44:49 +0800

    静态布局1差后台下右侧布局

--- my_project/routes/index.js
+++ my_project/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: '登录页面' });
+});
+router.get('/index', function(req, res, next) {
+  res.render('bam', { title: '后台管理页面' });
 });
 
 module.exports = router;
